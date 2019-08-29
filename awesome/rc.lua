cc2135135f12106b83423c0ba4a4b65448fa44e0
Zi Yan <ziy@nvidia.com>
2019-08-29 15:07:58 -0400

    do not allow mouse to change focus.
    Signed-off-by: Zi Yan <ziy@nvidia.com>

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -750,8 +750,16 @@ client.connect_signal("request::titlebars", function(c)
 end)
 
 -- Enable sloppy focus, so that focus follows mouse.
+--client.connect_signal("mouse::enter", function(c)
+    --c:emit_signal("request::activate", "mouse_enter", {raise = true})
+--end)
+
+-- Focus does not follow mouse hovers
 client.connect_signal("mouse::enter", function(c)
-    c:emit_signal("request::activate", "mouse_enter", {raise = true})
+      if awful.layout.get(c.screen) ~= awful.layout.suit.magnifier
+          and awful.client.focus.filter(c) then
+          client.focus = c
+      end
 end)
 
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
